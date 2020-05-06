d59987b4b56383bb8ca17ae6e1c9dee3e5d236ce
douglasdossantosdias <douglas.dsantos@rede.ulbra.br>
2020-05-06 10:11:36 -0300

    Adicionar arquivos de projeto.

---
+++ Sebrae_app/Controllers/LoginController.cs
@@ -0,0 +1,62 @@
+﻿using Sebrae_app.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Sebrae_app.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Login(string returnUrl)
+        {
+            ViewBag.ReturnUrl = returnUrl;
+            return View();
+          
+        }
+
+        [HttpPost]
+        public ActionResult Login(LoginViewModel model, string returnUrl)
+        {
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+
+                    //aqui a lógica de comparacao de login e senha
+                    Usuario u = new Usuario() ;
+                    u.NomeUsuario = "douglas";
+                    u.SenhaUsuario = "123456";
+
+
+                    if (model.NomeUsuario.Equals(u.NomeUsuario)&&model.SenhaUsuario.Equals(u.SenhaUsuario))
+                    {
+                        
+                        return RedirectToAction("Index", "Home");
+                    }
+                    else
+                    {
+                        ViewBag.Mensagem = "Acesso Negado.Tente novamente";
+                    }
+                }
+                catch (Exception)
+                {
+
+                    throw;
+                }
+            }
+
+            
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult LogOff()
+        {
+            return RedirectToAction("Login");
+        }
+    }
+}
---
+++ Sebrae_app/Models/Usuario.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Sebrae_app.Models
+{
+    public class Usuario
+    {
+        public int IdUsuario { get; set; }
+        [Required]
+        [Display(Name = "Login")]
+        public string NomeUsuario { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Senha")]
+        public string SenhaUsuario { get; set; }
+    }
+}
